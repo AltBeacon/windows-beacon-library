ac27c2583bf135a47e813f0a119da3ee63480f03
Furkan VAROL <furkanvarol@hotmail.com>
2015-03-04 18:21:31 +0200

    AltBeacon Library Test project has been created

---
+++ AltBeacon.LibraryTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AltBeacon.LibraryTest")]
+[assembly: AssemblyDescription("Windows Beacon Library Test")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Radius Networks, Inc.")]
+[assembly: AssemblyProduct("AltBeacon.LibraryTest")]
+[assembly: AssemblyCopyright("Copyright © 2015 Radius Networks")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5353e1f3-e621-43bd-9365-e47f2ef071d9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
